4799ddb21259a54b9c11ba3fa096768692a6b62f
JR Tashjian <jrtashjian@gmail.com>
2018-11-30 23:13:12 -0500

    Add default styles to deselect added styles.

--- src/blocks.js
+++ src/blocks.js
@@ -10,8 +10,22 @@ registerBlockStyle('core/paragraph', {
     isDefault: false,
 });
 
+// Default paragraph style for resetting block.
+registerBlockStyle('core/paragraph', {
+    name: 'default',
+    label: __('Default'),
+    isDefault: true,
+});
+
 registerBlockStyle('core/image', {
     name: 'spoiler-alert',
     label: __('Spoiler Alert'),
     isDefault: false,
+});
+
+// Default image style for resetting block.
+registerBlockStyle('core/image', {
+    name: 'default',
+    label: __('Default'),
+    isDefault: true,
 });
