2574f7f7fd36731d58aed1be28b9759d9e94f3cd
JR Tashjian <jrtashjian@gmail.com>
2018-11-30 23:05:43 -0500

    Simplifying styles and interaction.

--- src/sbb-spoiler-alert-theme.js
+++ src/sbb-spoiler-alert-theme.js
@@ -2,13 +2,9 @@
     "use strict";
 
     $( document ).ready( function() {
-        $( '.is-style-spoiler-alert' ).each( function( index, element ) {
-            if ( 'P' === element.nodeName ) {
-                $( element ).wrapInner( '<span></span>' );
-            }
+        $( '.is-style-spoiler-alert' ).click( function() {
+            $( this ).addClass( 'is-revealed' );
         } );
-
-        $( '.is-style-spoiler-alert' ).css( 'opacity', 1 );
     } );
 
 } )( jQuery );
