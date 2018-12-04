498b7952b200d149947652376770b60f7ae04da1
JR Tashjian <jrtashjian@gmail.com>
2018-12-04 11:52:14 -0500

    Bump version to 1.0.1

--- plugin.php
+++ plugin.php
@@ -5,7 +5,7 @@
  * Description: With Spoiler Alert you can stop ruining things for people on the internet.
  * Author: sorta brilliant
  * Author URI: https://sortabrilliant.com
- * Version: 1.0.0
+ * Version: 1.0.1
  * License: GPL2+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  *
